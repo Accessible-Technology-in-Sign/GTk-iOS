a74cff4dab59cea519e82585ef45d6f60c33f1ab
Ananay-22 <ananay@gatech.edu>
2024-12-30 11:28:48 +0530

    Initial Commit

---
+++ SLRGTk/SLRGTk.h
@@ -0,0 +1,18 @@
+//
+//  SLRGTk.h
+//  SLRGTk
+//
+//  Created by Ananay Gupta on 12/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SLRGTk.
+FOUNDATION_EXPORT double SLRGTkVersionNumber;
+
+//! Project version string for SLRGTk.
+FOUNDATION_EXPORT const unsigned char SLRGTkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLRGTk/PublicHeader.h>
+
+
---
+++ SLRGTkTests/SLRGTkTests.swift
@@ -0,0 +1,17 @@
+//
+//  SLRGTkTests.swift
+//  SLRGTkTests
+//
+//  Created by Ananay Gupta on 12/30/24.
+//
+
+import Testing
+@testable import SLRGTk
+
+struct SLRGTkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
